29410006159cb64c4b7e2d62ba83696ad5e3f606
thatstrangerwho <itrazjel@gmail.com>
2021-03-09 20:54:27 +0100

    All works now

--- caesars_affine.py
+++ caesars_affine.py
@@ -7,14 +7,17 @@
 #########################################################################
 # Author: A. Ekalt
 ###########
-
+import string
 import sys
+
+#for linear equations in finding affine cipher's key
+import numpy as np
 
 
 def input_reader():
     if len(sys.argv) < 3 or (sys.argv[1] != '-a' and sys.argv[1] != '-c') \
             or (sys.argv[2] != '-e' and sys.argv[2] != '-d' and sys.argv[2] != '-j' and sys.argv[2] != '-k'):
-        print('Compile the file writing the following after the name main.py: -c for Caesar\'s cipher '
+        print('Compile the file writing the following after the name caesars_affine.py: -c for Caesar\'s cipher '
               '\nor -a for affine cipher.' +
               '\nThen write -e for encryption or -d for decryption or -j for cryptanalysis with declared text' +
               '\nor -k for cryptanalysis without neither the text probe nor the key.')
@@ -152,26 +155,45 @@ def crypto_no_text(arg):
         print('Input file/-s not found')
 
 
-# not working for now
 def affine_crypto_text():
     with open('crypto.txt', 'r') as encoded_file:
         crypto = encoded_file.read()
     with open('extra.txt', 'r') as extra:
         example = extra.read()
-    if example >= 123 | example < 97:
-        print('Could not calculate the key')
-    key = 0
+    a = calc_key(crypto, example)[1]
+    b = calc_key(crypto, example)[0]
+    print(calc_key(crypto, example))
     new_text = ''
     for el in crypto:
         if ord(el) > 96 & ord(el) < 123:
-            new_text += chr((ord(el) - ord('a') - key) % 26 + ord('a'))
+            new_text += chr(pow(a, -1, 26) * (ord(el) - ord('a') - b) % 26 + ord('a'))
         else:
             new_text += el
     with open('key_found.txt', 'w') as key_file:
-        key_file.write(str(key))
+        key_file.write(' '.join([str(b), str(a)]))
     with open('decrypt.txt', 'w') as plain_file:
         plain_file.write(new_text)
     print('Decryption ready')
 
 
+def get_letter(letter):
+    return list(string.ascii_lowercase).index(letter)
+
+
+def calc_key(crypto, extra):
+    d_first = extra[0].lower()
+    d_second = extra[1].lower()
+    e_first = crypto[0]
+    e_second = crypto[1]
+    encrypted = (ord(e_first) - ord(e_second) + 26) % 26
+    decrypted = (ord(d_first) - ord(d_second) + 26) % 26
+    while (encrypted / decrypted) % 1 != 0:
+        encrypted += 26
+    key_a = int(encrypted / decrypted)
+    key_b = int((get_letter(e_first) - ((get_letter(d_first) * key_a) % 26) + 26) % 26)
+    return key_b, key_a
+
+
 input_reader()
+
+
